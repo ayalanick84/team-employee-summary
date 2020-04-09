06d4d8c07894a72dcb3d6c09d06d0432a0cc5e8b
Nicolas Ayala <ayalanick84@gmail.com>
2020-04-08 22:52:20 -0700

    Got it working and styled the page

--- Develop/app.js
+++ Develop/app.js
@@ -10,9 +10,8 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 newMember()
-const teamMembers = []
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const employees = []
+
 function newMember() {
 
     inquirer
@@ -40,14 +39,14 @@ function newMember() {
                     }, {
                         type: "input",
                         name: "email",
-                        message: "What is your email?"
+                        message: "What is their email?"
                     }, {
                         type: "input",
                         name: "officeNumber",
                         message: "What is their office number?"
                     }]).then(function (answers) {
                         const newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber, "Manager");
-                        teamMembers.push(newManager);
+                        employees.push(newManager);
                         newMember()
                     })
             }
@@ -64,14 +63,14 @@ function newMember() {
                     }, {
                         type: "input",
                         name: "email",
-                        message: "What is your email?"
+                        message: "What is their email?"
                     }, {
                         type: "input",
                         name: "github",
                         message: "What is their github username?"
                     }]).then(function (answers) {
                         const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github, "Engineer");
-                        teamMembers.push(newEngineer);
+                        employees.push(newEngineer);
                         newMember()
                     })
             }
@@ -89,43 +88,29 @@ function newMember() {
                     }, {
                         type: "input",
                         name: "email",
-                        message: "What is your email?"
+                        message: "What is their email?"
                     }, {
                         type: "input",
                         name: "school",
                         message: "What school are they attending?"
                     }]).then(function (answers) {
                         const newIntern = new Intern(answers.name, answers.id, answers.email, answers.school, "Manager");
-                        teamMembers.push(newIntern);
-                        
+                        employees.push(newIntern);
+
                         newMember()
                     })
             }
             else {
-                console.log(teamMembers)
+                fs.writeFile(outputPath,render(employees),function(err){
+                    if (err){throw err};
+                    console.log("success")
+                })
+                
             }
 
 
+        }).catch(error => {
+            console.log("Error")
         });
 
 }
-// After the user has input all employees desired, call the `render` function (required
-
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# team-employee-summary
+# Team Employee Summary
+[![License: MPL 2.0](https://img.shields.io/badge/License-None%20-brightgreen.svg)]
+## Description
+The point of this app is to make forming a team easy! You simply gather information about your team, answer the questions in the prompt and you will automatically be given a sheet with your teams information.
+
+## Installation
+To install the necessary dependencies, run the following commands
+npm install
+
+## Usage
+Go to my github to get the app.
+
+
+## Contributing
+No need to contribute as of now.
+
+## Tests 
+To run tests, run the following command
+npm run test
+## Questions
+If you have any questions, you could email me at ayalanick84@gmail.com, or message me on github https://api.github.com/users/ayalanick84
+![Github avatar](https://avatars3.githubusercontent.com/u/59402185?v=4)
+
